888db61d17599e3e987219e2f0e54b88543bdaf8
OJOJIN <ojy101039@gmail.com>
2023-11-20 21:40:18 +0900

    [feat] 최근 데이터 넘겨줄 때 날짜 시간 추가

--- src/main/java/com/gachon/crpytomarket/domain/history/dto/response/RecentTradeHistoryResponseDto.java
+++ src/main/java/com/gachon/crpytomarket/domain/history/dto/response/RecentTradeHistoryResponseDto.java
@@ -5,15 +5,20 @@ import com.gachon.crpytomarket.domain.history.entity.History;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.Date;
+
 @Getter
 @Builder
 public class RecentTradeHistoryResponseDto {
 
     private Integer price;
 
+    private Date timestamp;
+
     public static RecentTradeHistoryResponseDto of(History history) {
         return RecentTradeHistoryResponseDto.builder()
                 .price(history.getPrice())
+                .timestamp(history.getTimestamp())
                 .build();
     }
 }
